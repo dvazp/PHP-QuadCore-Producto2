9c3f1d3ff98810a5e7667997abe8d607f0f017d9
Davivp23 <davidvazpe23@gmail.com>
2026-04-09 13:42:36 +0200

    añadido debug en index.php para comprobar por qué desde el aws no funciona el logout

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $request = $_SERVER['REQUEST_URI'];
 
 $full_prefix = "https://fp064.techlab.uoc.edu/~uocx8";
