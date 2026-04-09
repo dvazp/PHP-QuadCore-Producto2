b148e722fd597b2939971543c4a191ebe4cd3558
Davivp23 <davidvazpe23@gmail.com>
2026-04-09 13:13:09 +0200

    eliminado die() de auth.php porque ya no se necesita debug, ya funciona correctamente.

--- controllers/auth.php
+++ controllers/auth.php
@@ -26,7 +26,6 @@ echo "Contenido de ese usuario en el array:\n";
 print_r($users[$username] ?? 'Nada encontrado');
 echo "\n¿La contraseña coincide?: " . (isset($users[$username]) && password_verify($password, $users[$username]['password']) ? 'SÍ' : 'NO');
 echo "</pre>";
-die(); // Paramos todo aquí para leer
 
 if (isset($users[$username]) && password_verify($password, $users[$username]['password'])) {
     // Autenticación OK
